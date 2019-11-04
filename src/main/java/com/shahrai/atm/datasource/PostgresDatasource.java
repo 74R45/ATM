58714a7b5837409538eca98cda79e387e8491f45
74R45 <mail2taras@gmail.com>
2019-11-04 17:52:22 +0200

    an actual working project

--- src/main/java/com/shahrai/atm/datasource/PostgresDatasource.java
+++ src/main/java/com/shahrai/atm/datasource/PostgresDatasource.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class PosgresDatasource {
+public class PostgresDatasource {
 
     @Bean
     @ConfigurationProperties("app.datasource")
